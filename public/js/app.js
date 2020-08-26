688c8d3ca6055a56178c56e3941fe496ec6e4a59
siriys1981 <4tai81@gmail.com>
2020-08-26 12:37:51 +0300

    cart added

--- public/js/app.js
+++ public/js/app.js
@@ -37341,8 +37341,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\user\Downloads\OSPanel\domains\my-shop\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\user\Downloads\OSPanel\domains\my-shop\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\HTML tasks\ШАГ\OpenServer\OSPanel\domains\from-home\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\HTML tasks\ШАГ\OpenServer\OSPanel\domains\from-home\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
